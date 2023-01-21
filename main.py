8a2ce0f01fc89e98d4fff7b5b0e08bef0c5005c7
Grinevich Elizabeth <nekomoneko477@gmail.com>
2023-01-21 11:00:12 +0200

    background, def path_file

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import pygame 
+import pygame
+import os
+pygame.init()
+
+
+def path_file(file_name):
+    folder_path = os.path.abspath(__file__ + "/..")
+    path = os.path.join(folder_path, file_name)
+    return path
+
 FPS = 40
 WIN_WIDTH = 700
 WIN_HEIGHT = 500
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
+
+background = pygame.image.load(path_file("backgroung.jpeg"))
+background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
+
 
 game = True
 play = True
